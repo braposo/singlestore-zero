35175ba416b614d133f42adbd9f788a2ed6eef1c
Bernardo Raposo <braposo@singlestore.com>
2021-03-18 02:04:45 +0000

    Uses ServerSideProps for create page

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,11 +1,11 @@
 import { pool } from "@src/utils/db";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 
 export default function Create() {
   return <div>Error creating new sheet</div>;
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const [rows] = await pool.execute(
     "select id from notes order by rand() LIMIT 1"
   );
